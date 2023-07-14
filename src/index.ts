dfdb1cb6e7db36222847b2ceb033bd01322951be
Michael Fanger <mvf4z7@users.noreply.github.com>
2023-07-14 01:05:13 -0400

    Minimally working implementation

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,55 @@
 // import { JWT } from "google-auth-library";
 
+import dotenv from "dotenv";
+import z from "zod";
+import { NestClient } from "./nest-client";
 import { SmartCocoonClient } from "./smart-cocoon-client";
 
 const NEST_DEVICE_ID = "6416660000E6DE0C";
 const PROJECT_ID = "a82a518f-da26-474b-90b9-6ba4a8877865";
 
 async function main() {
-  // const client = new JWT({
-  //   email: "smart-cocoon-integration@smart-co-392104.iam.gserviceaccount.com",
-  //   key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDJVD05Kc0rrVYL\nDo6qG8zmlVc1/e76VZjbHOZglrtsxNbKNLR3sNlD8QMfwZJUOpApYycVN6dRruhv\n04i449QwnsJGbNdHSvOSTQNmzzR5FW62+eEMcalPHzWo7xBOhMQBc86nKwQGwg3A\nM5XStoZtQUpo9aFQIZlFH1j00oeBhZpVxHhxhC6Z012HjLTXvjCq/rGhWajGEanV\nCABhG3mdWHZeyFbs0GVaIn3ApzoE2ReWKGqdZQWQLUTmeSZUALqurCWC4+5+T9uT\n/1gDt3eLUNXsDI3W6ZPMP8j/SSRRvX3i7GUrwhNzg6uj7EFjHr88UrC0YtjEazTt\nWu0hp5KRAgMBAAECggEAGiMTDa7vwWoFP8s52eYLtwBlVuTqwlocIuTAwy0JE7Ox\nlNpnIXDL9rDn2xa6GgWTqKPZiOd1k+UAAmCKdpexTkdAvnwr2J1nkLM/K/jeKD1l\nkbX6sa8MhXFDpJ+1/2jnZK7gVxMpDdLh48rD+nE9c2/5/9//phOuk8A1lWxthh1o\ngQSpAeDNnaOC1shijrIsyOjxUtV6JakTDFv2qpMEFMEixHFKC3/4bhVVQt0M+ixU\nECLGUPUeJE1l+bD1eE9gulg5oTsfBx1dYcghVAZ0lZHYzctDl1NertPCncLin00v\n2NNoOFGG1yes6P3MC1LrSd/Yw/s0UnI6R9BWmE+gzwKBgQD39Kcf9W8TdEcexHGi\n0yICDl0mTYRdYAwuuWgyqO9c0hYzAUsCjy0PgaL7x+CGlFThPCgREP8PN2yTRqTL\n50mSu3xdFvS0VfVzR8VVdS27wPtzmSrudBvy5X8tO00Zupr+W4jEbYSmJoV6Z8Ay\nB4bXoWowOPrP9mvEEqLBT5EgGwKBgQDP3FaPARxHAf05WjRjzUE+lNYSeQYaorVw\nRd+cavqsCIcs/KHY/kV0n/akyyhDcUePtMdpzzZamYmeOr0DudUbtlGMWcxoHwD5\nR+xtiaKS0uQNuuQbvrm6xZI7vypPstZRrFm+DDuJX4MXonFId7PPAj5w7m0ve/bi\nQKgPt+I6wwKBgH8VCBbsHrrUzKKrQiaWmYZ+QCWicyW8VWIZKptUVCG5lyEbF4u/\nP/QDmDn9LkkrevlZcJuWn3L4EBDHkyziZKXsq4e2fUJ0D/G+phfvj7n75aRQq109\naxmpn+3Zx+FY75lP5p/uFZ9mfODIjgvWKbkYhKkkev/dJ0x91KbJFfNjAoGAApW7\n4dVis6Nbsc3kXuVbjVquetZUdBpX5UiJKlnOOjt06uNNY/RtirC+kXgRTR1ohWn7\nHN69z0yfgH88gIJcGxceVn97YkQYcAEr+ITAp9DFk0IiuhGaH697TRDohsszjRp2\nlz1xtBii+bVEcWFDtCxT+HWOmAuTm2awcFmaD58CgYEArq6wuzpyjeG7xDIwW7zo\n21s57TFaXQkrZOCy9DyThbFRat6mdcupKO9QnusnFVZJ2odxn1LxAu2Q8pkdD5Ee\nj7YekWvLgBNF8tKDUIeOW9wUWNFnk2VtitwU+nJfC1FrBIoA49i1pH6OhW3Imkji\ntbwo6x39wu74l/wN6XHgyn8=\n-----END PRIVATE KEY-----\n",
-  //   scopes: ["https://www.googleapis.com/auth/sdm.service"],
-  // });
-  // const x = "/enterprises/project-id/devices/device-id";
-  // const url = `https://smartdevicemanagement.googleapis.com/v1/enterprises/${PROJECT_ID}/devices/${NEST_DEVICE_ID}`;
-  // const res = await client.request({ url });
-  // console.log(res.data);
-  // // After acquiring an access_token, you may want to check on the audience, expiration,
-  // // or original scopes requested.  You can do that with the `getTokenInfo` method.
-  // const tokenInfo = await client.getTokenInfo(
-  //   client.credentials.access_token ?? ""
-  // );
-  // console.log(tokenInfo);
+  dotenv.config();
+  const config = parseConfig(process.env);
 
-  const smartCocoonClient = await SmartCocoonClient.authenticate({
-    email: "mike.fanger@gmail.com",
-    password: process.env.SMART_COCOON_PASSWORD!,
-  });
+  const [smartCocoonClient, nestClient] = await Promise.all([
+    SmartCocoonClient.authenticate({
+      email: config.smartCocoon.email,
+      password: config.smartCocoon.password,
+    }),
+    NestClient.authenticate({
+      clientId: config.google.oauthClientId,
+      clientSecret: config.google.oauthClientSecret,
+      refreshToken: config.google.refreshToken,
+    }),
+  ]);
 
-  const fans = await smartCocoonClient.listFans();
-  console.log(fans);
+  const [fans, thermostat] = await Promise.all([
+    smartCocoonClient.listFans(),
+    nestClient.getThermostat(
+      config.google.deviceAccessProjectId,
+      config.google.deviceId
+    ),
+  ]);
 
-  await smartCocoonClient.updateFan(fans[0].id, { mode: "eco" });
+  for (const fan of fans) {
+    const hvacStatus =
+      thermostat.traits["sdm.devices.traits.ThermostatHvac"].status;
+    const fanStatus = thermostat.traits["sdm.devices.traits.Fan"].timerMode;
+
+    const heatingOrCooling =
+      hvacStatus === "COOLING" || hvacStatus === "HEATING";
+    const hvacFanRunning = fanStatus === "ON";
+    const hvacOn = heatingOrCooling || hvacFanRunning;
+
+    const fanOn = fan.fan_on;
+
+    if (hvacOn && !fanOn) {
+      await smartCocoonClient.updateFan(fan.id, { mode: "always_on" });
+    } else if (!hvacOn && fanOn && fan.mode !== "eco") {
+      await smartCocoonClient.updateFan(fan.id, { mode: "eco" });
+    }
+  }
 }
 
 main().catch(console.error);
@@ -39,3 +58,45 @@ main().catch(console.error);
  * authorization link
  * https://nestservices.google.com/partnerconnections/a82a518f-da26-474b-90b9-6ba4a8877865/auth?redirect_uri=http://localhost&access_type=offline&prompt=consent&client_id=103949787762591375139&response_type=code&scope=https://www.googleapis.com/auth/sdm.service
  */
+
+interface Config {
+  google: {
+    deviceAccessProjectId: string;
+    deviceId: string;
+    oauthClientId: string;
+    oauthClientSecret: string;
+    refreshToken: string;
+  };
+  smartCocoon: {
+    email: string;
+    password: string;
+  };
+}
+
+function parseConfig(environemnt: NodeJS.Process["env"]): Config {
+  const environmentSchema = z.object({
+    GOOGLE_DEVICE_ACCESS_PROJECT_ID: z.string(),
+    GOOGLE_DEVICE_ID: z.string(),
+    GOOGLE_OAUTH_CLIENT_ID: z.string(),
+    GOOGLE_OAUTH_CLIENT_SECRET: z.string(),
+    GOOGLE_REFRESH_TOKEN: z.string(),
+    SMART_COCOON_EMAIL: z.string(),
+    SMART_COCOON_PASSWORD: z.string(),
+  });
+
+  const parsedEnvironment = environmentSchema.parse(process.env);
+
+  return {
+    google: {
+      deviceAccessProjectId: parsedEnvironment.GOOGLE_DEVICE_ACCESS_PROJECT_ID,
+      deviceId: parsedEnvironment.GOOGLE_DEVICE_ID,
+      oauthClientId: parsedEnvironment.GOOGLE_OAUTH_CLIENT_ID,
+      oauthClientSecret: parsedEnvironment.GOOGLE_OAUTH_CLIENT_SECRET,
+      refreshToken: parsedEnvironment.GOOGLE_REFRESH_TOKEN,
+    },
+    smartCocoon: {
+      email: parsedEnvironment.SMART_COCOON_EMAIL,
+      password: parsedEnvironment.SMART_COCOON_PASSWORD,
+    },
+  };
+}
--- src/nest-client.ts
+++ src/nest-client.ts
@@ -1,10 +1,99 @@
+import axios, { AxiosInstance } from "axios";
 export class NestClient {
-  readonly refreshToken: string;
-  constructor(refreshToken: string) {
-    this.refreshToken = refreshToken;
+  private readonly accessToken: string;
+  private readonly axiosInstance: AxiosInstance;
+
+  private constructor(accessToken: string) {
+    this.accessToken = accessToken;
+    this.axiosInstance = axios.create({
+      baseURL: "https://smartdevicemanagement.googleapis.com",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
   }
 
-  async authorize(): Promise<void> {
-    return;
+  async getThermostat(
+    projectId: string,
+    deviceId: string
+  ): Promise<ThermostatDevice> {
+    const url = `/v1/enterprises/${projectId}/devices/${deviceId}`;
+    const response = await this.axiosInstance.get<ThermostatDevice>(url);
+
+    return response.data;
   }
+
+  static async authenticate({
+    clientId,
+    clientSecret,
+    refreshToken,
+  }: AuthenticationData): Promise<NestClient> {
+    const tokenRefreshUrl = `https://www.googleapis.com/oauth2/v4/token?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
+    const response = await axios.post<RefreshTokenResponse>(tokenRefreshUrl);
+
+    return new NestClient(response.data.access_token);
+  }
+}
+
+export interface AuthenticationData {
+  clientId: string;
+  clientSecret: string;
+  refreshToken: string;
+}
+
+interface RefreshTokenResponse {
+  access_token: string;
+  expires_in: number;
+  scope: string;
+  token_type: string;
+}
+
+interface ThermostatDevice {
+  name: string;
+  type: "sdm.devices.types.THERMOSTAT";
+  assignee: string;
+  traits: {
+    "sdm.devices.traits.Info": {
+      customName: string;
+    };
+    "sdm.devices.traits.Humidity": {
+      ambientHumidityPercent: number;
+    };
+    "sdm.devices.traits.Connectivity": {
+      status: "OFFLINE" | "ONLINE";
+    };
+    "sdm.devices.traits.Fan": {
+      timerMode: "ON" | "OFF";
+      timerTimeout: string;
+    };
+    "sdm.devices.traits.ThermostatMode": {
+      mode: "HEAT" | "COOL" | "HEATCOOL" | "OFF";
+      availableModes: ("HEAT" | "COOL" | "HEATCOOL" | "OFF")[];
+    };
+    "sdm.devices.traits.ThermostatEco": {
+      availableModes: ("OFF" | "MANUAL_ECO")[];
+      mode: "OFF" | "MANUAL_ECO";
+      heatCelsius: number;
+      coolCelsius: number;
+    };
+    "sdm.devices.traits.ThermostatHvac": {
+      status: "OFF" | "HEATING" | "COOLING";
+    };
+    "sdm.devices.traits.Settings": {
+      temperatureScale: "FAHRENHEIT" | "CELSIUS";
+    };
+    "sdm.devices.traits.ThermostatTemperatureSetpoint": {
+      coolCelsius?: number;
+      heatCelsius?: number;
+    };
+    "sdm.devices.traits.Temperature": {
+      ambientTemperatureCelsius: number;
+    };
+  };
+  parentRelations: [
+    {
+      parent: string;
+      displayName: string;
+    }
+  ];
 }
